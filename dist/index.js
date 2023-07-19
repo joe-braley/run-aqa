61409204edf2eef8ed4d32871f583dfe059750b1
joebraley <joebraley@microsoft.com>
2023-07-19 09:49:06 -0500

    run pack and build

--- dist/index.js
+++ dist/index.js
@@ -414,8 +414,8 @@ function getAqaTestsRepo(aqatestsRepo, version, buildList) {
         if (IS_WINDOWS && buildList != '') {
             if (buildList === 'system') {
                 process.chdir('system');
-                yield exec.exec(`git clone -q -b v0.9.7 https://github.com/adoptium/aqa-systemtest.git`); // points to master
-                yield exec.exec(`git clone -q -b v0.9.7 https://github.com/adoptium/STF.git`); // points to master
+                yield exec.exec(`git clone -q -b v0.9.8 https://github.com/adoptium/aqa-systemtest.git`); // points to v0.9.7
+                yield exec.exec(`git clone -q -b v0.9.8 https://github.com/adoptium/STF.git`); // points to v0.9.7
                 process.chdir('../');
             }
             if (buildList === 'openjdk' && version != '') {
@@ -423,10 +423,10 @@ function getAqaTestsRepo(aqatestsRepo, version, buildList) {
                 let jdkBranch = '';
                 // Shallow clone the adoptium JDK version - quietly - if there is a reference repo obtain objects from there - destination is openjdk-jdk
                 if (version === "11") {
-                    jdkBranch = "jdk-11.0.19+6";
+                    jdkBranch = "jdk-11.0.20+8";
                 }
                 else {
-                    jdkBranch = "jdk-17.0.7+6";
+                    jdkBranch = "jdk-17.0.8+7";
                 }
                 yield exec.exec(`git clone --depth 1 -q -b ${jdkBranch} --reference-if-able ${process.env.GITHUB_WORKSPACE}/openjdk_cache https://github.com/adoptium/jdk${version}.git openjdk-jdk`);
                 process.chdir('../');
